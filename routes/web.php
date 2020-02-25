49ead273b0c479eb577e81070b1aa8fe3958fbe6
wavewcy <35341804+wavewcy@users.noreply.github.com>
2020-02-25 18:23:36 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+
+
+
