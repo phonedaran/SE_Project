4c6a5ab675f3d9da6709f7cb0b4470ce100748d1
wavewcy <35341804+wavewcy@users.noreply.github.com>
2020-03-02 17:45:33 +0700

    admin #1

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\DB;
 
 class adminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function admin(){
         $tutors = DB::table('tutor')->where(['status' => 'waiting'])->get();
         $idCards = DB::table('image')->get();
@@ -16,14 +21,25 @@ class adminController extends Controller
     public function accepted(request $request){
         $idTutor = $request->input('idTutor');
         DB::table('tutor')->where(['idTutor'=>$idTutor])->update(['status'=>'accepted']);
-        return redirect()->back(); 
+        $Fname = DB::table('tutor')->where(['idTutor'=>$idTutor])->value('Fname');
+        $email = DB::table('tutor')->where(['idTutor'=>$idTutor])->value('email');
+        $password = DB::table('tutor')->where(['idTutor'=>$idTutor])->value('password');
+        print_r($Fname);
+        DB::table('users')->insert([
+            'id' => $idTutor,
+            'name' => $Fname,
+            'email' => $email,
+            'status' => 'status',
+            'password' => $password
+        ]);
+        return redirect()->back();
     }
+
     public function rejected(request $request){
         $idTutor = $request->input('idTutor');
         DB::table('image')->where(['idTutor'=>$idTutor])->delete();
         DB::table('course')->where(['idTutor'=>$idTutor])->delete();
         DB::table('tutor')->where(['idTutor'=>$idTutor])->delete();
-        DB::table('course')->where(['idTutor'=>$idTutor])->delete();
         return redirect()->back(); 
     }
 
