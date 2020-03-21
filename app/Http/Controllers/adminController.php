d2273c649ae839f2623fbea349156072299315cb
lydiawimoo <35342723+lydiawimoo@users.noreply.github.com>
2020-03-21 11:12:45 +0700

    images

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -53,4 +53,5 @@ class adminController extends Controller
         $idCards = DB::table('image')->get();
         return view('admin/tutorList',['tutors' => $tutors,'idCards' => $idCards]);
     }
+    
 }
